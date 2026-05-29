9f49f82191aec1989e2fec622722a0c83a9d7b2d
= <rahyandesign@gmail.com>
2026-05-29 13:04:10 +0800

    #04 Tipe Dasar dan Inferensi

---
+++ 04-inferensi/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let nama = "Rahyan"; // Inferensi tipe data string
+// Ganti nilai nama dengan tipe string
+nama = "Fauzan";
+// Ganti nilai nama dengan tipe number
+nama = 21;
+console.log(nama);
---
+++ 04-inferensi/index.ts
@@ -0,0 +1,13 @@
+let nama = "Rahyan"; // Inferensi tipe data string
+
+// Ganti nilai nama dengan tipe string
+nama = "Fauzan";
+
+// Ganti nilai nama dengan tipe number
+// nama = 21;
+
+let bekerja = true; // tipe boolean
+
+// bekerja = "ya"; // tipe string (tidak sesuai)
+
+console.log(nama);
