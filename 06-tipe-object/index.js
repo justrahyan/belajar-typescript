50e747979213f3607f1bef69b79725bd2ce2de45
= <rahyandesign@gmail.com>
2026-06-11 22:53:33 +0800

    #06 Tipe Object

---
+++ 06-tipe-object/index.js
@@ -0,0 +1,15 @@
+"use strict";
+let karyawan = {
+    nama: "Rahyan",
+    umur: 20,
+    aktif: true,
+};
+// karyawan.jenisKelamin = "Laki-Laki";
+// karyawan.nama = "Fauzan";
+// karyawan.nama = 12;
+karyawan = {
+    nama: "Fauzan",
+    umur: 21,
+    aktif: true,
+};
+console.log(karyawan);
