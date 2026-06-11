2c63848d14899fd435edd81b8de27164b345cf95
= <rahyandesign@gmail.com>
2026-06-11 22:24:32 +0800

    #05 Tipe Array

---
+++ 05-tipe-array/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// const angka = [1, 2, 3]
+// angka.push(4)
+// angka.push("5")
+// console.log(angka)
+const orang = ["Rahyan", 20]; // tipe array of string or number
+orang.push("Ini String");
+orang.push(21);
+// orang.push(true);
+orang[0] = "Rahyan 2";
+orang[0] = 21;
+// orang[1] = true;
+console.log(orang);
