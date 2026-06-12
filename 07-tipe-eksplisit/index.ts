2deec06649a8cf6ba2dc22be6eb93a3e8885950d
= <rahyandesign@gmail.com>
2026-06-12 22:01:23 +0800

    #07 Tipe Eksplisit

---
+++ 07-tipe-eksplisit/index.ts
@@ -0,0 +1,12 @@
+// tipe  yang ditentukan secara implisit = inferensi
+// let nama = "Rahyan";
+
+// tipe yang ditentukan secara eksplisit
+let nama: string = "Rahyan";
+
+let umur: number;
+umur = 20;
+
+// tipe data array yang ditentukan secara eksplisit
+let kegiatan: string[] = [];
+kegiatan.push("ngoding");
