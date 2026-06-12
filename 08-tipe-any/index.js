75dc7080c41ab6457265b8342bd2e9648f6133db
= <rahyandesign@gmail.com>
2026-06-12 22:23:13 +0800

    #08 Tipe Any

---
+++ 08-tipe-any/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let nama;
+nama = "Rahyan";
+nama = 20;
+nama = ["satu", "dua", 3, 4, false, true, []];
+nama = { a: 1, b: false };
---
+++ 08-tipe-any/index.ts
@@ -0,0 +1,13 @@
+let nama: any;
+
+nama = "Rahyan";
+nama = 20;
+nama = ["satu", "dua", 3, 4, false, true, []];
+nama = { a: 1, b: false};
+
+nama.toUpperCase();
+nama.length;
+nama.push(10);
+nama.tidakAda;
+nama.satu.dua.tiga().empat;
+nama.sukaSuka();
